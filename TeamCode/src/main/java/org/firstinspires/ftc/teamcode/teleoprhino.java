daf0c7056da6e65ad3ecb74e675ae395c3d16417
Alex <alexky.gan@gmail.com>
2023-11-23 12:38:58 -0500

    teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoprhino.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoprhino.java
@@ -23,13 +23,13 @@ public class teleoprhino extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("BackLeft"); //1
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("FrontRight"); //2
         DcMotor motorBackRight = hardwareMap.dcMotor.get("BackRight"); //3
-        Servo cone = hardwareMap.servo.get("cone"); //0
-        Servo knocker = hardwareMap.servo.get("knocker"); //1
-        //expansion
-        Servo lswing = hardwareMap.servo.get("lswing"); //port 0 lswing
-        Servo rswing = hardwareMap.servo.get("rswing"); //port 1 rswing
-        Servo claw = hardwareMap.servo.get("claw"); //port 2 claw
-        DcMotor slides = hardwareMap.dcMotor.get("slides"); //0
+//        Servo cone = hardwareMap.servo.get("cone"); //0
+//        Servo knocker = hardwareMap.servo.get("knocker"); //1
+//        //expansion
+//        Servo lswing = hardwareMap.servo.get("lswing"); //port 0 lswing
+//        Servo rswing = hardwareMap.servo.get("rswing"); //port 1 rswing
+//        Servo claw = hardwareMap.servo.get("claw"); //port 2 claw
+//        DcMotor slides = hardwareMap.dcMotor.get("slides"); //0
 
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -44,7 +44,8 @@ public class teleoprhino extends LinearOpMode {
         while (opModeIsActive()) {
             //finally moving the motors
             if (gamepad1.left_stick_y == 0 && gamepad1.left_stick_x ==0){
-                motorFrontLeft.setPower(0);
+                motorFrontLeft.setPower(0
+                );
                 motorBackLeft.setPower(0);
                 motorFrontRight.setPower(0);
                 motorBackRight.setPower(0);
@@ -74,34 +75,34 @@ public class teleoprhino extends LinearOpMode {
                 motorFrontLeft.setPower(1);
 
             }
-            if (gamepad2.left_stick_y > 0){
-                slides.setPower(gamepad2.left_stick_y);
-            }
-            else if (gamepad2.left_stick_y < 0){
-                slides.setPower(gamepad2.left_stick_y);
-            }
-            slides.setPower(0);
-            if (gamepad2.dpad_up){
-                lswing.setPosition(lswing.getPosition()+0.1);
-                rswing.setPosition(lswing.getPosition()-0.1);
-            }
-            else if (gamepad2.dpad_down){
-                lswing.setPosition(lswing.getPosition()-20);
-                rswing.setPosition(lswing.getPosition()-20);
-            }
-            if (gamepad2.left_trigger > 0){
-                cone.setPosition(cone.getPosition()+0.1);
-            }
-            else{
-                cone.setPosition(cone.getPosition()-0.1);
-            }
-
-            if (gamepad2.left_bumper){
-                claw.setPosition(cone.getPosition()+1);
-            }
-            else if (gamepad2.right_bumper){
-                claw.setPosition(cone.getPosition()-1);
-            }
+//            if (gamepad2.left_stick_y > 0){
+//                slides.setPower(gamepad2.left_stick_y);
+//            }
+//            else if (gamepad2.left_stick_y < 0){
+//                slides.setPower(gamepad2.left_stick_y);
+//            }
+//            slides.setPower(0);
+//            if (gamepad2.dpad_up){
+//                lswing.setPosition(lswing.getPosition()+0.1);
+//                rswing.setPosition(lswing.getPosition()-0.1);
+//            }
+//            else if (gamepad2.dpad_down){
+//                lswing.setPosition(lswing.getPosition()-20);
+//                rswing.setPosition(lswing.getPosition()-20);
+//            }
+//            if (gamepad2.left_trigger > 0){
+//                cone.setPosition(cone.getPosition()+0.1);
+//            }
+//            else{
+//                cone.setPosition(cone.getPosition()-0.1);
+//            }
+//
+//            if (gamepad2.left_bumper){
+//                claw.setPosition(cone.getPosition()+1);
+//            }
+//            else if (gamepad2.right_bumper){
+//                claw.setPosition(cone.getPosition()-1);
+//            }
         }
     }
 }
