fc6b58d6a2049260f51f4424cd08db00e622b42c
Alex <alexky.gan@gmail.com>
2023-11-26 14:54:17 -0500

    11/26/23 adjustable speed joystick thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRhino.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRhino.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp
 //@Disabled
@@ -17,6 +18,8 @@ public class TeleopRhino extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         //motors
         //test 10/29/22
+        final double dampSpeedRatio = 0.85;
+        final double dampTurnRatio = 0.8;
         DcMotor motorFrontLeft = hardwareMap.dcMotor.get("FrontLeft"); //0
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("BackLeft"); //1
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("FrontRight"); //2
@@ -24,14 +27,16 @@ public class TeleopRhino extends LinearOpMode {
 //        Servo cone = hardwareMap.servo.get("cone"); //0
 //        Servo knocker = hardwareMap.servo.get("knocker"); //1
 //        //expansion
-//        Servo lswing = hardwareMap.servo.get("lswing"); //port 0 lswing
-//        Servo rswing = hardwareMap.servo.get("rswing"); //port 1 rswing
+        Servo leftarm = hardwareMap.servo.get("leftarm"); //port 0 lswing
+        Servo rightarm = hardwareMap.servo.get("rightarm"); //port 1 rswing
 //        Servo claw = hardwareMap.servo.get("claw"); //port 2 claw
-       // DcMotor slides = hardwareMap.dcMotor.get("slides"); //0
+        DcMotor slides = hardwareMap.dcMotor.get("slides"); //0
 
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        leftarm.setPosition(0.28);
+        rightarm.setPosition(0.72);
 
 
         //wait for button click
@@ -39,17 +44,56 @@ public class TeleopRhino extends LinearOpMode {
         // stop the program if button click
         if (isStopRequested()) return;
         boolean slowMode = false;
-        while (opModeIsActive()) {
-            //finally moving the motors
-//            if (gamepad1.dpad_up){
-//                slides.setPower(0.5);
-//
-//            }
-//            if (gamepad1.dpad_down){
-//                slides.setPower(-0.5);
-//
-//            }
 
+        while (opModeIsActive()) {
+            telemetry.addData("leftarm", Double.toString(leftarm.getPosition()));
+            telemetry.addData("rightarm", Double.toString(rightarm.getPosition()));
+
+            if (gamepad2.a){
+                leftarm.setPosition(leftarm.getPosition()+0.01);
+                rightarm.setPosition(rightarm.getPosition()-0.01);
+            }
+            if (gamepad2.b){
+                leftarm.setPosition(leftarm.getPosition()-0.01);
+                rightarm.setPosition(rightarm.getPosition()+0.01);
+            }
+            if (gamepad2.left_bumper){
+                leftarm.setPosition(0.49);
+                rightarm.setPosition(0.51);
+            }
+
+
+            slides.setPower(gamepad2.left_stick_y);
+
+            double y = Range.clip(-gamepad1.left_stick_y, -1, 1);
+            //left stick x value
+            double x = Range.clip(-gamepad1.left_stick_x, -1, 1);
+            //right stick x value
+            double rx = Range.clip(-gamepad1.right_stick_x, -1, 1);
+
+
+            double flPower = (y - x) * dampSpeedRatio + dampTurnRatio * rx;
+            double frPower = (y + x) * dampSpeedRatio - dampTurnRatio * rx;
+            double blPower = (y + x) * dampSpeedRatio + dampTurnRatio * rx;
+            double brPower = (y - x) * dampSpeedRatio - dampTurnRatio * rx;
+
+            double maxFront = Math.max(flPower, frPower);
+            double maxBack = Math.max(blPower, brPower);
+            double maxPower = Math.max(maxFront, maxBack);
+
+            if (maxPower > 1.0) {
+                flPower /= maxPower;
+                frPower /= maxPower;
+                blPower /= maxPower;
+                brPower /= maxPower;
+            }
+            //finally moving the motors
+            motorFrontLeft.setPower(flPower);
+            motorBackLeft.setPower(blPower);
+            motorFrontRight.setPower(frPower);
+            motorBackRight.setPower(brPower);
+            // all the movement controls
+            /*
             if (gamepad1.left_stick_y == 0 && gamepad1.left_stick_x ==0){
                 motorFrontLeft.setPower(0);
                 motorBackLeft.setPower(0);
@@ -81,10 +125,10 @@ public class TeleopRhino extends LinearOpMode {
                 motorBackRight.setPower(0.5);
             }
             else if (gamepad1.left_stick_x != 0 && gamepad1.left_stick_x > 0){
-                motorFrontLeft.setPower(0.5);
-                motorBackLeft.setPower(-0.5);
-                motorFrontRight.setPower(-0.5);
-                motorBackRight.setPower(0.5);
+                motorFrontLeft.setPower(-0.5);
+                motorBackLeft.setPower(0.5);
+                motorFrontRight.setPower(0.5);
+                motorBackRight.setPower(-0.5);
 
             }
             if (gamepad1.right_stick_x > 0 && gamepad1.right_stick_x != 0){
@@ -99,7 +143,8 @@ public class TeleopRhino extends LinearOpMode {
                 motorFrontRight.setPower(-0.5);
                 motorBackRight.setPower(-0.5);
             }
-
+            telemetry.update();
+*/
 // not useful for now
             //            if (gamepad2.left_stick_y > 0){
 //                slides.setPower(gamepad2.left_stick_y);
