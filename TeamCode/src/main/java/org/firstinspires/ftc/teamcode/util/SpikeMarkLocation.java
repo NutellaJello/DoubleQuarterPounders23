01964c99ba6038198b3b3c5e6f85faf810a33cdc
chengyugan <chengyu.gan@gmail.com>
2023-12-31 13:15:37 -0500

    add turnByDegrees without gyro

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/SpikeMarkLocation.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Simple enum to keep track of the location of the spike marker.
+ */
+public enum SpikeMarkLocation {
+    LEFT,
+    CENTER,
+    RIGHT;
+
+}
